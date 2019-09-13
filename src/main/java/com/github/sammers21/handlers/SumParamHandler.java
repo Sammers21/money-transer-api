23109dd356a4669a88a1aa626dbb8b2e993f1585
Sammers21 <titantins@gmail.com>
2019-09-13 10:14:01 +0300

    starting from the scratch

--- src/main/java/com/github/sammers21/handlers/SumParamHandler.java
+++
@@ -1,6 +0,0 @@
-package com.github.sammers21.handlers;
-
-public interface SumParamHandler {
-
-  void handle(Long sum);
-}
---
+++ src/main/java/com/github/sammers21/money/transfer/Main.java
@@ -0,0 +1,10 @@
+package com.github.sammers21.money.transfer;
+
+import io.vertx.reactivex.core.Vertx;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+  }
+}
